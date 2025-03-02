60fc1fa6cdf2e3772b848320feb4b0a4a5504c0c
Huy <mrhuydoduc@gmail.com>
2025-03-02 13:09:02 +0100

    update main.py for useing pyside

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
 import sys
-import PySide6.QtCore
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QtCore, QtWidgets, QtGui
-                                                     
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    label = QLabel("Hello World", alignment=Qt.Alignment.AlignCenter)
-    label.show()
-    print(PySide6.QtCore.__version__)
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
     sys.exit(app.exec())
